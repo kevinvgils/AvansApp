2f28c1a6cd8e4fd0d405abd720cd3d76f4a1ea11
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2022-06-02 10:33:46 +0200

    Added home button
    Made the Avans logo clickable, directing to homepage.

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,8 @@
 <header>
     <div class="wrap">
-        <img src="img/Logo.png" alt="Logo">
+        <a href="index.php">
+          <img src="img/Logo.png" alt="Logo">
+        </a>
         <ul>
           <li>Login</li>
         </ul>
