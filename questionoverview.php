c0fbc1b0de225ce48665695f7120cc34a58530da
N1TROGUE <mohammedbogatyrev@gmail.com>
2022-06-08 16:03:29 +0200

    Removed bootstrap modal and added list of questions matching with styling

--- questionoverview.php
+++ questionoverview.php
@@ -11,22 +11,20 @@ include("./dataaccess/questionData.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/72d4739e76.js" crossorigin="anonymous"></script>
     <title>AvansApp</title>
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <?php $show_overview_button =  "<button type=\"button\" class=\"btn btn-light\" data-bs-toggle=\"modal\" data-bs-target=\"#exampleModal\"><i class=\"fa-solid fa-bars\"></i></button>";
-    include("templates/header.php"); ?>
+    <?php include("templates/header.php"); ?>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header bg-danger">
-                    <h5 class="modal-title text-white" id="exampleModalLabel">
+    <div class="wrap">
+        <div class="item">
+            <div class="itemWrap">
+
+                <div class="itemHeader">
+
+                    <h3>
                         <?php include("./dataaccess/databaseconnection.php");
                         // $somevar = $_SESSION["routeId"];
                         $somevar = 1;
@@ -39,43 +37,47 @@ include("./dataaccess/questionData.php");
                                 echo $route->routeName;
                             }
                         } ?>
-                    </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </h3>
+
                 </div>
-                <div class="modal-body">
-                    <h6 class="font-weight-bold">Globale vragen</h6>
-                    <?php
-                    // $somevar = $_SESSION["routeId"];
-                    $somevar = 1;
-                    $query = "SELECT * FROM `question` WHERE `longitude` IS NULL AND `routeId` = " . $somevar;
-
-                    $stm = $con->prepare($query);
-                    if ($stm->execute()) {
-                        $result = $stm->fetchAll(PDO::FETCH_OBJ);
-                        foreach ($result as $questions) {
-                            echo $questions->question;
-                            echo "<br/>";
+                <div class="itemContent">
+                    <strong>Globale vragen</strong>
+                    <p>
+                        <?php
+                        // $somevar = $_SESSION["routeId"];
+                        $somevar = 1;
+                        $questionCount = 1;
+                        $query = "SELECT * FROM `question` WHERE `longitude` IS NULL AND `routeId` = " . $somevar;
+                        $stm = $con->prepare($query);
+                        if ($stm->execute()) {
+                            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+                            foreach ($result as $questions) {
+                                echo $questionCount++ . ". ";
+                                echo $questions->question;
+                                echo "<br/>";
+                            }
                         }
-                    }
-                    ?>
-
+                        ?>
+                    </p>
                     <br>
-
-                    <h6 class="font-weight-bold">Locatie vragen</h6>
-                    <?php
-                    // $somevar = $_SESSION["routeId"];
-                    $somevar = 1;
-                    $query = "SELECT * FROM `question` WHERE `longitude` IS NOT NULL AND `routeId` = " . $somevar;
-
-                    $stm = $con->prepare($query);
-                    if ($stm->execute()) {
-                        $result = $stm->fetchAll(PDO::FETCH_OBJ);
-                        foreach ($result as $questions) {
-                            echo $questions->question;
-                            echo "<br/>";
+                    <strong>Locatie vragen</strong>
+                    <p>
+                        <?php
+                        // $somevar = $_SESSION["routeId"];
+                        $somevar = 1;
+                        $questionCount = 1;
+                        $query = "SELECT * FROM `question` WHERE `longitude` IS NOT NULL AND `routeId` = " . $somevar;
+                        $stm = $con->prepare($query);
+                        if ($stm->execute()) {
+                            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+                            foreach ($result as $questions) {
+                                echo $questionCount++ . ". ";
+                                echo $questions->question;
+                                echo "<br/>";
+                            }
                         }
-                    }
-                    ?>
+                        ?>
+                    </p>
                 </div>
             </div>
         </div>
--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,5 @@
         <a href="index.php">
           <img src="img/Logo.png" alt="Logo">
         </a>
-        <?php $show_overview_button; 
-        if (isset($show_overview_button)) {echo $show_overview_button;}?>
     </div>
 </header>
