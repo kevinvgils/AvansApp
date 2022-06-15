055f91ddc4ad994a903e4d50534434d115761239
Dimitri-de-bie <dimitriproject3@gmail.com>
2022-06-15 12:35:56 +0200

    archiveren toegevoegd

--- dataaccess/routeData.php
+++ dataaccess/routeData.php
@@ -13,10 +13,10 @@ function addRoutes($routeName, $course, $desc, $picture)
     $stm->execute();
 }
 
-function getAllRoutes()
+function getAllActiveRoutes()
 {
     include("databaseconnection.php");
-    $allRoutesQuery = "SELECT * FROM `route`";
+    $allRoutesQuery = "SELECT * FROM `route` WHERE `isActive` = 1";
     $stm = $con->prepare($allRoutesQuery);
     if ($stm->execute()) {
         $allRoutes = $stm->fetchAll(PDO::FETCH_OBJ);
@@ -27,7 +27,7 @@ function getAllRoutes()
 function getAllRoutesByCourse($courseId)
 {
     include("databaseconnection.php");
-    $allRoutesQuery = "SELECT * FROM `route` WHERE `courseId` = :courseId";
+    $allRoutesQuery = "SELECT * FROM `route` WHERE `courseId` = :courseId AND `isActive` = 1;";
     $stm = $con->prepare($allRoutesQuery);
     $stm->bindValue(':courseId', $courseId);
 
@@ -35,5 +35,17 @@ function getAllRoutesByCourse($courseId)
         $allRoutes = $stm->fetchAll(PDO::FETCH_OBJ);
     }
 
+    return $allRoutes;
+}
+
+function archiveRoute($routeId)
+{
+    include("databaseconnection.php");
+    $allRoutesQuery = "UPDATE `route` SET `isActive` = '0' WHERE `route`.`routeId` = :routeId;";
+    $stm = $con->prepare($allRoutesQuery);
+    $stm->bindValue(':routeId', $routeId);
+    if ($stm->execute()) {
+        $allRoutes = $stm->fetchAll(PDO::FETCH_OBJ);
+    }
     return $allRoutes;
 }
