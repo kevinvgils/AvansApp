78b09877025f37e810c7e65f3818235a69ed9a1b
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2022-06-20 13:40:00 +0200

    Start on keeping collapsible open (GEEN PR MAKEN)
    GEEN PULL REQUEST MAKEN. Ben nog bezig, maar wilde alvast pushen zodat anderen het konden zien of eraan werken.

--- admin/runningroutes.php
+++ admin/runningroutes.php
@@ -170,13 +170,7 @@ if (isset($_POST["button"])) {
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
+            this.parentElement.classList.toggle("active");
         });
     }
 </script>
