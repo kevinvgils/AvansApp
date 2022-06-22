786a1e2f8f99ae152b1a8801f6d4e0c1e9776473
BBosgraaf <bryanbosgraaf@live.nl>
2022-06-22 14:03:57 +0200

    added favicon

--- startroute.php
+++ startroute.php
@@ -39,6 +39,7 @@ if (isset($_POST["btnOpslaan"])) {
 <html lang="en">
 
 <head>
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
